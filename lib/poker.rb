742a6fd490fb59b53962875fbc9cd505f082aae6
David <dbravo7@yahoo.com>
2019-04-11 11:13:05 -0400

    Created files for initial classes

---
+++ lib/poker.rb
@@ -0,0 +1,11 @@
+require 'byebug'
+
+class PokerGame
+
+
+
+end 
+
+if $PROGRAM_NAME == __FILE__
+	PokerGame.new
+	end 
---
+++ spec/poker_spec.rb
@@ -0,0 +1,18 @@
+require 'poker'
+
+# Classes you will want
+  # Card : value, revealed=false, to_s
+  # Deck : shuffle, deal 
+  # Hand
+    # The logic of calculating pair, three-of-a-kind, two-pair, etc. goes here.
+    # Logic of which hand beats which would go here.
+
+#   Player
+      # Each player has a hand, plus a pot
+      # Player has methods to ask the user:
+        # Which cards they wish to discard
+        # Whether they wish to fold, see, or raise.
+  # Game
+      # Holds the deck
+      # Keeps track of whose turn it is
+      # Keeps track of the amount in the pot.
